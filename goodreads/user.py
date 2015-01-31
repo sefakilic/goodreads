5a95bfd0a53546d44382922e331c2fc6e9ea286d
Sefa Kilic <sefakilic@gmail.com>
2015-01-30 21:45:18 -0500

    user methods added

--- goodreads/user.py
+++ goodreads/user.py
@@ -2,6 +2,38 @@ from request import GoodreadsRequest
 
 class GoodreadsUser():
     def __init__(self, user_dict):
-        for key in user_dict:
-            setattr(self, key, user_dict[key])
+        self._user_dict = user_dict
+
+    def __repr__(self):
+        return self.username
+
+    @property
+    def id(self):
+        """Goodreads ID for the user"""
+        return self._user_dict['id']
+
+    @property
+    def user_name(self):
+        """Goodreads handle of the user"""
+        return self._user_dict['user_name']
+
+    @property
+    def name(self):
+        """Name of the user"""
+        return self._user_dict['name']
+
+    @property
+    def link(self):
+        """URL for user profile"""
+        return self._user_dict['link']
+
+    @property
+    def image_url(self):
+        """URL of user image"""
+        return self._user_dict['image_url']
+
+    @property
+    def small_image_url(self):
+        """URL of user image (small)"""
+        return self._user_dict['small_image_url']
 
