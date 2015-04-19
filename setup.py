bac7890ca1fcedd7312be2865e7e08d23f2aca74
Sefa Kilic <sefakilic@gmail.com>
2015-04-18 22:52:59 -0400

    README format change

--- setup.py
+++ setup.py
@@ -1,21 +1,13 @@
 from setuptools import setup
-import pypandoc
-
-try:
-    from pypandoc import convert
-    read_md = lambda f: convert(f, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
 
 setup(
     name = 'goodreads',
     description = "Python wrapper for Goodreads API",
-    long_description = read_md("README.md")
+    long_description = open("README.rst").read(),
     author = 'Sefa Kilic',
     author_email = 'sefakilic@gmail.com',
     url = 'https://github.com/sefakilic/goodreads/',
-    version = '0.2.2',
+    version = '0.2.3',
     install_requires = ['nose', 'xmltodict', 'requests', 'rauth'],
     packages = ['goodreads'],
     scripts = [],
