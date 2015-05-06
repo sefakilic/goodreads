73808fcda3f1769eddb1ed2c26908f4f3a4e0caa
Sefa Kilic <sefakilic@gmail.com>
2015-05-06 18:46:11 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name = 'goodreads',
-    description = "Python wrapper for Goodreads API",
-    long_description = open("README.rst").read(),
-    author = 'Sefa Kilic',
-    author_email = 'sefakilic@gmail.com',
-    url = 'https://github.com/sefakilic/goodreads/',
-    version = '0.2.3',
-    install_requires = ['nose', 'xmltodict', 'requests', 'rauth'],
-    packages = ['goodreads'],
-    scripts = [],
-    license = 'MIT',
+    name='goodreads',
+    description="Python wrapper for Goodreads API",
+    long_description=open("README.rst").read(),
+    author='Sefa Kilic',
+    author_email='sefakilic@gmail.com',
+    url='https://github.com/sefakilic/goodreads/',
+    version='0.2.4',
+    install_requires=['nose', 'xmltodict', 'requests', 'rauth'],
+    packages=['goodreads'],
+    scripts=[],
+    license='MIT',
 )
